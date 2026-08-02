4dd7d9a4ed5428980a68ac17fb270feceeca22c4
Sghizzus <ivanmarino@live.it>
2026-08-02 16:42:38 +0200

    Ho sostituito DBI con pool che mi sembra un modo migliore per connettersi da un app

--- R/db_connect.R
+++ R/db_connect.R
@@ -1,10 +1,12 @@
-library(DBI)
+library(pool)
 library(RPostgres)
 
 #' Connessione al database PostgreSQL su Supabase
 #'
 #' Stabilisce una connessione al database PostgreSQL ospitato su Supabase
 #' utilizzando le credenziali configurate.
+#'
+#' @param password la password per connettersi al database, letta di default automatico da DB_PWD
 #'
 #' @details La password viene letta dalla variabile d'ambiente `DB_PWD`.
 #'   Assicurarsi che sia impostata prima di chiamare la funzione.
@@ -19,13 +21,13 @@ library(RPostgres)
 #' dbDisconnect(con)
 #' }
 
-db_connect <- function() {
-  dbConnect(
+db_connect <- function(password = Sys.getenv("DB_PWD")) {
+  dbPool(
     RPostgres::Postgres(),
     dbname = "postgres",
     host = "aws-1-eu-west-3.pooler.supabase.com",
     port = 5432,
     user = "postgres.pntkrsospmzbuyelbmac",
-    password = Sys.getenv("DB_PWD")
+    password = password
   )
 }
