d953d98e06b0c5cb9d0f04fe6fe8fb286bd30111
Sghizzus <ivanmarino@live.it>
2026-09-05 23:30:52 +0200

    Creato script di aggiornamento che verrà triggerato con tempi casuali

--- run.R
+++ run.R
@@ -1,34 +1,45 @@
 library(logger)
+library(DBI)
+library(RPostgres)
 
-source("R/db_connect.R")
 source("R/otp_polling.R")
 source("R/scarica_excel.R")
 source("R/aggiorna_db.R")
 
-con <- db_connect()
+con <- dbPool(
+  Postgres(),
+  dbname = "postgres",
+  host = "aws-1-eu-west-3.pooler.supabase.com",
+  port = 5432,
+  user = "postgres.pntkrsospmzbuyelbmac",
+  password = Sys.getenv("DB_PWD")
+)
 
 # Leggo il prossimo orario pianificato dal db
-next_run <- dbGetQuery(con, "SELECT next_run FROM scheduler WHERE id = 1")$next_run
-now <- Sys.time()
+next_run <- tbl(con, "scheduler") |>
+  pull(next_run) |>
+  with_tz("Europe/Rome")
+
+now <- now()
 
 log_info("Ora attuale:          {format(now)}")
 log_info("Prossima esecuzione:  {format(next_run)}")
 
 if (now < next_run) {
   log_info("Troppo presto, esco.")
-  poolClose(con)
+  dbDisconnect(con)
   quit(save = "no", status = 0)
 }
 
 log_info("Avvio aggiornamento...")
 
 scarica_excel(con)
-aggiorna_db()
+aggiorna_db(con)
 
 # Aggiorno il prossimo orario: aggiungo ore casuali con distribuzione
-# esponenziale di parametro 1/4 (media attesa: 4 ore)
-delay_hours <- rexp(1, rate = 1 / 4)
-new_next_run <- next_run + delay_hours * 3600
+# esponenziale di parametro 1/sqrt(24) (media attesa: sqrt(24) ore)
+delay_hours <- rexp(1, rate = 1 / sqrt(24))
+new_next_run <- next_run + dhours(delay_hours)
 
 dbExecute(
   con,
@@ -38,4 +49,4 @@ dbExecute(
 
 log_info("Completato. Prossima esecuzione: {format(new_next_run)}")
 
-poolClose(con)
+dbDisconnect(con)
