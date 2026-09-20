ed6997bb02a2fea1e61d4dd7176801f57884309d
Sghizzus <ivanmarino@live.it>
2026-09-20 23:35:39 +0200

    File reformat

--- app.R
+++ app.R
@@ -24,10 +24,18 @@ fmt_eur <- label_currency(
 )
 
 mesi_italiani <- c(
-  "1" = "Gennaio", "2" = "Febbraio",  "3" = "Marzo",
-  "4" = "Aprile",  "5" = "Maggio",    "6" = "Giugno",
-  "7" = "Luglio",  "8" = "Agosto",    "9" = "Settembre",
-  "10" = "Ottobre","11" = "Novembre", "12" = "Dicembre"
+  "1" = "Gennaio",
+  "2" = "Febbraio",
+  "3" = "Marzo",
+  "4" = "Aprile",
+  "5" = "Maggio",
+  "6" = "Giugno",
+  "7" = "Luglio",
+  "8" = "Agosto",
+  "9" = "Settembre",
+  "10" = "Ottobre",
+  "11" = "Novembre",
+  "12" = "Dicembre"
 )
 
 # --- UI ---
